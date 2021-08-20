d2e298a2cbf9decb387698e16e0400055865f5ca
Rajan Thakkar <43638170+RAJAN8553@users.noreply.github.com>
2021-08-20 11:55:01 +0530

    Data engineer
    kindly add a database before running this code and also one table name "test".

--- table_create.py
+++ table_create.py
@@ -7,68 +7,73 @@ cursor = connection.cursor()
 if connection:
     print("DATABASE CONNECT SUCCESSFULLY ")
 
-# open file
+# open file in read mode
 f = open("data_20210819.txt", 'r')
 row = f.readlines()
 
 
+# create function for insert D type of Data
 def insert_record(split_data):
-    s = "INSERT INTO `" + country + "`(`Customer Name`, `Customer ID`, " \
-                                      "`Customer Open Date`, `Last Consulted Date`," \
-                                      " `Vaccination Type`, `Doctor Consulted`," \
-                                      " `State`, `Country`," \
-                                      " `Date of Birth`, `Active Customer`)" \
-                                      " VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+    s = "INSERT INTO `" + split_data[9] + "`(`Customer Name`, `Customer ID`, " \
+                                    "`Customer Open Date`, `Last Consulted Date`," \
+                                    " `Vaccination Type`, `Doctor Consulted`," \
+                                    " `State`, `Country`," \
+                                    " `Date of Birth`, `Active Customer`)" \
+                                    " VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
     v = split_data[2::1]
-    print("value ", v)
     cursor.execute(s, v)
-    print("Record Data Successfully ")
+    print("Record Your Data Successfully. \n Name Of customer ", split_data[2])
 
+
+# main program
 
 for i in range(0, len(row)):
-    split = row[i].split("|")
-    flag = 0
-    if split[1].lower() == 'h':
-        sql = "INSERT INTO `test` (`Customer Name`, `Customer Open Date`, `Customer ID`) VALUES (%s,%s,%s);"
-        val = split[2:(len(split[i]) - 1)]
-        print("value ", val)
-        # executing the quires
+    split = row[i].split("|")  # Split file data to list
+    flag = 0  # This variable for check country table
+
+    if split[1].lower() == 'h':  # this condition check H type of data and insert record
+        # Header record will store in test table
+        sql = "INSERT INTO `test` (`Customer Name`, `Customer Open Date`, `Customer ID`) " \
+                                  "VALUES (%s,%s,%s);"  # sql query
+        val = split[2:(len(split[i]) - 1)]  # use indexing for store data
+        # executing the query
         cursor.execute(sql, val)
 
-    elif split[1].lower() == 'd':
-        country = split[9]
-        sql = "SHOW TABLES"
-        cursor.execute(sql)
-        for tb in cursor:
-            print(tb)
-            print(country)
-            if country.lower() in tb:
-                flag = 1
+    elif split[1].lower() == 'd':  # condition for store Detail Data
+        sql = "SHOW TABLES"  # sql query
+        cursor.execute(sql)  # execute query
 
-        split[10] = datetime.strptime(split[10], '%m%d%Y')
+        for tb in cursor:  # store all table of hospital database
+            if split[9].lower() in tb:  # checks if same country is in table
+                flag = 1
+        split[10] = datetime.strptime(split[10], '%m%d%Y')  # format our given date of birth
+
         if flag:
-            insert_record(split)
+            insert_record(split)  # call the function
 
         else:
-
-            sql = "CREATE TABLE `hospital`.`" + country + "`( `Customer Name` VARCHAR(255) NOT NULL ," \
-                            " `Customer ID` VARCHAR(18) NOT NULL , `Customer Open Date` DATE NOT NULL , " \
-                            "`Last Consulted Date` DATE NULL , `Vaccination Type` CHAR(5) NULL , " \
-                            "`Doctor Consulted` CHAR(255) NULL , `State` CHAR(5) NULL , " \
-                            "`Country` CHAR(5) NULL , `Post Code` INT(5) NULL , " \
-                            "`Date of Birth` DATE NULL , `Active Customer` CHAR(1) NULL , " \
-                            "PRIMARY KEY (`Customer Name`)) ;"
+            # else it create new country table and store data into new country table
+            sql = "CREATE TABLE `hospital`.`" + split[9] + "`( `Customer Name` VARCHAR(255) NOT NULL ," \
+                                                          " `Customer ID` VARCHAR(18) NOT NULL , " \
+                                                          "`Customer Open Date` DATE NOT NULL , " \
+                                                          "`Last Consulted Date` DATE NULL ," \
+                                                          " `Vaccination Type` CHAR(5) NULL , " \
+                                                          "`Doctor Consulted` CHAR(255) NULL , `State` CHAR(5) NULL ," \
+                                                          "`Country` CHAR(5) NULL , `Post Code` INT(5) NULL , " \
+                                                          "`Date of Birth` DATE NULL , " \
+                                                          "`Active Customer` CHAR(1) NULL , " \
+                                                          "PRIMARY KEY (`Customer Name`)) ;"
             cursor.execute(sql)
-            insert_record(split)
+            insert_record(split)  # call the function
 
-    elif split[1].lower() == 't':
+    elif split[1].lower() == 't':  # store T type of data
         sql = "INSERT INTO `test` (`Customer ID`)" \
               " VALUES (%s);"
-        print("Split", split)
 
         val = split[2::2]
-        print("value ", val)
         cursor.execute(sql, val)
 
-connection.commit()
-connection.close()
+    else:
+        print("Please give valid mode of record ")
+connection.commit()  # committing the current transaction
+connection.close()  # close the connection
